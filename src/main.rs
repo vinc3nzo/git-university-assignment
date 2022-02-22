cbca1279f47194bdc555b555cd4b2b1c1c8d63ec
vinc3nzo <mangasaryan.ep@gmail.com>
2022-02-22 21:41:23 +0400

    Change program, so that it computes n-th Fibonacci number iteratively, in a loop.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-fn fib(n: u32) -> u128 {
+fn fib(n: u32) {
     if n == 0 { panic!("shouldn't have done that") }
-    if n == 1 { 0 }
-    else if n == 2 { 1 }
-    else { fib(n - 1) + fib(n - 2) }
+    if n >= 1 { println!("0"); }
+    if n >= 2 { println!("1"); }
+
+    let mut x: u128 = 0;
+    let mut y: u128 = 1;
+    let mut res: u128;
+    for _ in 2..n {
+        res = x + y;
+        x = y;
+        y = res;
+        println!("{}", res);
+    }
 }
 
 fn main() {
-    println!("{}", fib(10));
+    fib(10);
 }
