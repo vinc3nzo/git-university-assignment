a8c4fcf437edbd120e78f3ef149f7989a65eadc2
vinc3nzo <mangasaryan.ep@gmail.com>
2022-02-22 21:19:29 +0400

    Change the `fib(n)` function in such a way, that "Fibonacci" sequence starts with (0, 1, 1, 2, ...).

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 fn fib(n: u32) -> u128 {
     if n == 0 { panic!("shouldn't have done that") }
-    if n == 1 || n == 2 { return 1 }
-    fib(n - 1) + fib(n - 2)
+    if n == 1 { 0 }
+    else if n == 2 { 1 }
+    else { fib(n - 1) + fib(n - 2) }
 }
 
 fn main() {
