e43d4779eac051578b1f7395185a29889da07dbb
vinc3nzo <mangasaryan.ep@gmail.com>
2022-02-22 21:15:14 +0400

    Add the new function `fib(n)`, that recursively computes n-th Fibonacci number.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn fib(n: u32) -> u128 {
+    if n == 0 { panic!("shouldn't have done that") }
+    if n == 1 || n == 2 { return 1 }
+    fib(n - 1) + fib(n - 2)
+}
+
 fn main() {
-    println!("Hello, World!");
+    println!("{}", fib(10));
 }
